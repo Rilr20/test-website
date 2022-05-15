a1f4f0010d34c37d2f295b334088f86f48c0fa23
Rilr20 <nrrl99@gmail.com>
2022-05-15 16:58:17 +0200

    changes to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import OSRSTable from "../src/components/osrstable";
+// import { getOsrs } from "../src/api/getOsrs";
 import { LayoutOne } from "./template/LayoutOne";
 
 Home.PageTitle = 'Home | Website'
-Home.Layout = LayoutOne 
+Home.Layout = LayoutOne
 export default function Home() {
-  return (
-    <Container maxWidth="xl" sx={{mt:2}}>
-        <h1>Hello</h1>
-    </Container>
-  )
+    return (
+        <Container maxWidth="xl" sx={{ mt: 2 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box sx={{ width: "50%", backgroundColor: "lime", height: "400px", margin: "0.5em", padding: "0.5em" }}>
+                    <Typography variant="h3" sx={{textAlign:"center"}}>Hello</Typography>
+                </Box>
+                <Box sx={{ width: "50%", backgroundColor: "orange", height: "400px", margin: "0.5em", padding: "0.5em", display:"flex" }}>
+                    <Box sx={{ backgroundColor: "",height: "100%", width:"200px" }}>
+                        <OSRSTable />
+                        </Box>
+                    <Box sx={{ backgroundColor: "blue", height: "200px", width:"200px" }}></Box>
+                </Box>
+            </Box>
+        </Container>
+    )
 }

b95466c3fc816f0e0b6d90e14494787cc4cfe48d
Rilr20 <nrrl99@gmail.com>
2022-05-18 00:02:03 +0200

    responsiveness for mobile

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { Container, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Container, Typography, Card } from "@mui/material";
+import { Box, typography } from "@mui/system";
 import OSRSTable from "../src/components/osrstable";
 import LayoutBase from "../src/template/layoutbase";
 // import { getOsrs } from "../src/api/getOsrs";
@@ -9,18 +9,19 @@ Home.PageTitle = 'Home | Website'
 // Home.Layout = LayoutOne
 export default function Home() {
     return (
-        <Container maxWidth="xl" sx={{ mt: 2 }}>
-            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                <Box sx={{ width: "50%", backgroundColor: "lime", height: "400px", margin: "0.5em", padding: "0.5em" }}>
+        <Container maxWidth="xl" sx={{ height:"100%", pt: 2,mt:0.3,  }}>
+            <Box cla sx={{ display: {md: "flex", xs:"block"}, justifyContent: "space-between" }}>
+                <Box sx={{ width: {md:"50%", xs:"100%"}, backgroundColor: "", height: "auto", margin: "0.5em", padding: "0.5em" }}>
                     <Typography variant="h3" sx={{ textAlign: "center" }}>Hello</Typography>
                 </Box>
-                <Box sx={{ width: "50%", backgroundColor: "orange", height: "400px", margin: "0.5em", padding: "0.5em", display: "flex" }}>
-                    <Box sx={{ backgroundColor: "blue", height: "200px", width: "100%" }}>
-
+                <Box sx={{ justifyContent:"center", width: { md: "50%", xs: "100%" }, backgroundColor: "", height: "400px", margin: "0.5em", padding: "0.5em", display: {md: "flex", xs:"initial"} }}>
+                    <Box sx={{ backgroundColor: "blue", height: "200px", minWidth:"200px", maxWidth: "100%", mx:2, flexGrow:1 }}>
+                        
                     </Box>
-                    <Box sx={{ backgroundColor: "", height: "100%", width: "200px" }}>
+                    <Card sx={{ height: "auto", p: "1rem", backgroundColor: theme => `${theme.palette.secondary.superlight}`, mx: 2  }}>
+                        <Typography sx={{mx:1, textAlign:"center"}} variant="h5">OSRS Stats</Typography>
                         <OSRSTable />
-                    </Box>
+                    </Card>
 
                 </Box>
             </Box>

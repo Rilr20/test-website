9303bbfc8c22e22f696c01f32220b062e76af98b
Rilr20 <nrrl99@gmail.com>
2022-06-06 10:43:11 +0100

    unit test and functions that pass

---
+++ src/components/bowlinglogic.js
@@ -0,0 +1,92 @@
+import { getFormControlUnstyledUtilityClasses } from "@mui/base"
+
+const bowlinglogic = {
+    calculateScore: function (bowlingArray) {
+        let points = 0
+        let check;
+        let checkAgain;
+        let looptimes = 0;
+        for (let i = 0; i < bowlingArray.length; i++) {
+            checkAgain = ""
+            looptimes = 0
+            check = checkNext(bowlingArray[i])
+            switch (check) {
+                case "strike":
+                        if (bowlingArray[i].length === 3) {
+                            points += calculateFrame(bowlingArray[i])
+                        } else {
+                            points = points + 10
+                            if (i < bowlingArray.length - 1) {
+                                if (bowlingArray[i+1].length === 3) {
+                                    points = points + bowlingArray[i+1][0] + bowlingArray[i+1][1]
+                                } else {
+                                    points += calculateFrame(bowlingArray[i+1])
+                                    checkAgain = checkNext(bowlingArray[i + 1])
+                                }
+                            }
+                            if (i < bowlingArray.length - 2 && checkAgain == "strike") {
+                                console.log("real shit");
+                                points += bowlingArray[i + 2][0]
+
+                            }
+                        }
+                    break;
+                case "spare":
+                    if (bowlingArray[i].length === 3 ) {
+                        points += calculateFrame(bowlingArray[i])
+                    } else {
+                        points = points + 10
+                        if (i + 1 < bowlingArray.length) {
+                            points = points + bowlingArray[i + 1][0]
+                        }
+                    }
+                    break;
+                default:
+                    console.log(calculateFrame(bowlingArray[i]));
+                    points += calculateFrame(bowlingArray[i])
+                    break;
+            }
+        }
+        return points
+    },
+    displayScoreBoardPoints: function (bowlingArray) {
+        let returnArray
+        // [["", "X"], ["5", "/"]]
+        return returnArray
+    },
+    checkNext: function (bowlingArray) {
+        if (bowlingArray[0] === 10) {
+            return "strike"
+        }
+        if (bowlingArray[0] + bowlingArray[1] === 10) {
+            return "spare"
+        }
+        return null
+    },
+    calculateFrame: function (frame) {
+        let points = 0;
+        switch (frame.length) {
+            case 1:
+                return frame[0]
+            case 2:
+                for (let i = 0; i < frame.length; i++) {
+                    points += frame[i];
+                }
+                break;
+
+            case 3:
+                for (let i = 0; i < frame.length - 1; i++) {
+                    points += frame[i]
+                }
+                if (points >= 10) {
+                    points += frame[2]
+                }
+                break;
+        }
+        return points
+    }
+}
+export default bowlinglogic
+export const calculateScore = bowlinglogic.calculateScore
+export const calculateFrame = bowlinglogic.calculateFrame
+export const checkNext = bowlinglogic.checkNext
---
+++ test/bowling.test.js
@@ -0,0 +1,83 @@
+import bowlinglogic, { calculateFrame } from "../src/components/bowlinglogic";
+
+describe('bowling table frame tests', () => {
+    test('calculate frame normal', () => {
+        let frame = [2, 3]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(5)
+    });
+    test('calculate frame spare 2 8', () => {
+        let frame = [2, 8]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(10)
+    });
+    test('calculate frame 0 10', () => {
+        let frame = [0, 10]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(10)
+    })
+    test('calcuate frame strike', () => {
+        let frame = [10]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(10)
+    });
+    test('calculate frame misses', () => {
+        let frame = [0, 0]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(0)
+    })
+    test('calclate last frame three strikes', () => {
+        let frame = [10, 10, 10]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(30)
+    })
+    test('calculate last frame impossible', () => {
+        let frame = [2, 5, 2]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(7)
+    })
+    test('calculate last frame spare', () => {
+        let frame = [2, 8, 5]
+        let res = bowlinglogic.calculateFrame(frame)
+        expect(res).toBe(15)
+    })
+});
+describe('bowling frame scoretable test', () => {
+    test('max score', () => {
+        let scoretable = [[10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10, 10]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(300)
+    });
+    test('full game scoring two scores last round', () => {
+        let scoretable = [[2, 4], [9, 1], [10], [7, 2], [0, 10], [10], [10], [10], [4, 1], [3, 5]]
+        // let scoretable = [[2,4],[9,1],[10], [7,2]]
+        // let scoretable = [[0, 10], [10], [10], [10], [4, 1]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(156)
+    })
+    test('full game only spares', () => {
+        let scoretable = [[9,1], [9,1], [9,1], [9,1], [9,1], [9,1], [9,1], [9,1], [9, 1], [9, 1, 9]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(190)
+    })
+    test('last three with spare', () => {
+        let scoretable = [[0,10,5]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(15)
+    })
+    test('one strikes in row', () => {
+        let scoretable = [[10]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(10)
+    });
+    test('two strikes in row', () => {
+        let scoretable = [[10], [10]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(30)
+    });
+    test('three strikes in row', () => {
+        let scoretable = [[10],[10],[10]]
+        let res = bowlinglogic.calculateScore(scoretable)
+        expect(res).toBe(60)
+    });
+});

7bdd87224f17dd5e63b9b09d90a1b1cd6d6048d7
Rilr20 <nrrl99@gmail.com>
2022-06-20 23:25:19 +0200

    removed stuff

--- src/components/osrstable.js
+++ src/components/osrstable.js
@@ -1,28 +1,12 @@
-import { SaveAs } from '@mui/icons-material';
 import { Grid } from '@mui/material'
-import { AppBar, Container, Drawer, IconButton, Toolbar, Typography } from '@mui/material';
-import Image from 'next/image';
 import React from 'react'
-import { useEffect, useState } from "react";
 import OSRSCard from './osrscard';
 
-
 export default function OSRSTable(props) {
-const [string, setString] = useState(null);
-    const [stuffz, setStuff] = useState("")
-    // const [osrs, setOsrs] = useState(placeholder)
-    // console.log(props)
-    useEffect(() => {
-        // if (stuffz === placeholder) {
-        //     // addToDict(osrs)
-        // }
-    }, []);
-
     return (
         <Grid className="osrs" container spacing={0.1} sx={{ display:"grid", textAlign: "center", gap: 0.2, gridTemplateColumns: 'repeat(3, 50px)', p:"0.5em", justifyContent: "center" }}>
             {
                 props.osrs.map(function (skill, i) {
-                    // console.log(skill)
                     return <Grid key={i} sx={{ border: "1px solid black", borderRadius: "5px", backgroundColor: "#494035" }} item>
                         <OSRSCard skill={skill.skill} xp={skill.xp} lvl={skill.lvl}></OSRSCard>
                     </Grid>
@@ -31,4 +15,3 @@ const [string, setString] = useState(null);
         </Grid>
     )
 }
-

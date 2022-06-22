65600c5879210af1c46a8843d4364ab5e4f8e518
Rilr20 <nrrl99@gmail.com>
2022-06-22 19:45:26 +0200

    optional small width

--- src/components/customcard.js
+++ src/components/customcard.js
@@ -2,7 +2,8 @@ import { Box } from '@mui/material'
 import React from 'react'
 
 Customcard.defaultProps = {
-    top:"30px",
+    top: "30px",
+    smallWidth:"300px",
 }
 
 export default function Customcard(props) {
@@ -15,8 +16,8 @@ export default function Customcard(props) {
     }
     let top = getTopValue(props.height, props.top)
     return (
-        <Box className="card-shadow" sx={{ backgroundColor: theme => `${theme.palette.secondary.superlight}`, width: props.width, height: props.height, position: "relative", borderTopLeftRadius: "10px", borderBottomLeftRadius: "4px", borderBottomRightRadius: "4px", borderTopRightRadius: "10px", m: "auto", mt: 2, }}>
-            <Box sx={{ backgroundColor: theme => `${theme.palette.primary.superlight}`, width: props.width, height: top, position: "absolute", bottom: 0, textAlign: "center", borderRadius: "8px 8px 4px 4px" }}>
+        <Box className="card-shadow" sx={{ backgroundColor: theme => `${theme.palette.secondary.superlight}`, width: {md:props.width, xs:props.smallWidth}, height: props.height, position: "relative", borderTopLeftRadius: "10px", borderBottomLeftRadius: "4px", borderBottomRightRadius: "4px", borderTopRightRadius: "10px", m: "auto", mt: 2, }}>
+            <Box sx={{ backgroundColor: theme => `${theme.palette.primary.superlight}`, width: {md:props.width, xs:props.smallWidth}, height: top, position: "absolute", bottom: 0, textAlign: "center", borderRadius: "8px 8px 4px 4px" }}>
                 {props.children}
             </Box>
         </Box>

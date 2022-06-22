88b14369091a30037147c08927f081c8f8332a81
Rilr20 <nrrl99@gmail.com>
2022-06-22 19:45:48 +0200

    aligns text left

--- pages/about.js
+++ pages/about.js
@@ -32,7 +32,7 @@ export default function About({ data }) {
     }, []);
     return (
         <Container maxWidth="xl" sx={{ width: "auto", height: "100%", pt: 2, mt: 0.3, color: "text" }}>
-            <Box sx={{ width: "auto", height: "100vh", backgroundColor: "orange", p: 1, display: "flex", justifyContent: "space-between", flexDirection: "row-reverse" }}>
+            <Box sx={{ width: "auto", height: "100vh", backgroundColor: "orange", p: 1, display: "flex", justifyContent: "space-between", flexDirection: {md:"row-reverse", xs:"column"} }}>
                 <Customcard width="500px" height="300px">
                     {/* <Box sx={{ backgroundImage: `url(/img/default.avif)`, width:"100%",height:"250px"}}></Box> */}
                     <Box sx={{ display: "flex" }}>
@@ -44,16 +44,16 @@ export default function About({ data }) {
                         </div>
                         <Box sx={{m:2, width:"300px", height:"100%"}}>
                             <Typography variant="h1" sx={{ fontSize: "32pt", textAlign:"center" }}>About</Typography>
-                            <Typography>Sida för skojs skull, lägger upp en massa skit. </Typography>
-                            <Typography>Gjord med Nextjs, och annat gott</Typography>
+                            <Typography sx={{textAlign:"left"}}>Sida för skojs skull, lägger upp en massa skit. </Typography>
+                            <Typography sx={{textAlign:"left"}}>Gjord med Nextjs, och annat gott</Typography>
                         </Box>
                     </Box>
                 </Customcard>
                 <Customcard width="350px" height="300px">
-                    <Typography variant="h1" sx={{ mt: 2, fontSize:"32pt" }}>
+                    <Typography variant="h1" sx={{ mt: 2, fontSize:"32pt", width:"90%", mx:"auto" }}>
                         Olika Stiler
                     </Typography>
-                    <Typography>Denna sidan (/about) borde ändra sig beroende på vädret, med hjälp av ett väder API. Andra saker som har gjorts är ett tärningsspel, och ett poängräknare för bowling</Typography>
+                    <Typography sx={{ width: "90%", mx: "auto", textAlign:"left" }}>Denna sidan (/about) borde ändra sig beroende på vädret, med hjälp av ett väder API. Andra saker som har gjorts är ett tärningsspel, och ett poängräknare för bowling</Typography>
 
                 </Customcard>
 

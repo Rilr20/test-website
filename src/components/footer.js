9e8c8a91b8bdd60afcc78c5b32eea24f4933647d
Rilr20 <nrrl99@gmail.com>
2022-06-23 23:27:01 +0200

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <Box sx={{color:"text.primary"}}>
+          <div>Link to stuff or something idk</div>
+          <div>&copy; 2022 &gt;:)</div>
+          <div>skapad av mich</div>
+          {/* <Typography variant="h1" sx={{textAlign:"center"}}>🇬🇧 🇸🇪 🇪🇺</Typography> */}
+    </Box>
+  )
+}

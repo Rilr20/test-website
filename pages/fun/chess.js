ee1549a6576036c691ee7b5f9b7f433734b3cb0f
Rilr20 <nrrl99@gmail.com>
2022-07-07 23:00:37 +0200

    removed test svg

--- pages/fun/chess.js
+++ pages/fun/chess.js
@@ -15,18 +15,6 @@ export default function Chess() {
     return (
         <Container sx={{ width: "96%", height: "100%", pt: 2, mt: 0.3, }}>
             <Box sx={{ m: "auto", width: "400px", height: "100%", backgroundColor:"orange"}}>
-                {/* <PieceSVG piece="king" colour="white" size="55" />
-                <PieceSVG piece="king" colour="black" size="55" />
-                <PieceSVG piece="knight" colour="white" size="55" />
-                <PieceSVG piece="knight" colour="black" size="55" />
-                <PieceSVG piece="rook" colour="white" size="55" />
-                <PieceSVG piece="rook" colour="black" size="55" />
-                <PieceSVG piece="bishop" colour="white" size="55" />
-                <PieceSVG piece="bishop" colour="black" size="55" />
-                <PieceSVG piece="pawn" colour="white" size="55" />
-                <PieceSVG piece="pawn" colour="black" size="55" />
-                <PieceSVG piece="queen" colour="white" size="55" />
-                <PieceSVG piece="queen" colour="black" size="55" /> */}
                 <Chessboard width="45px" colour="white" pieceSize="55" chessBoard={chessBoard} setChessBoard={setChessBoard} removedPieces={removedPieces} setRemovedPieces={setRemovedPieces} />
             </Box>
         </Container>
